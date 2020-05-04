9f6cc169039c23be3ae752b16aa978112eebbe11
dramforever <dramforever@live.com>
2020-05-04 23:53:27 +0800

    process: Minor fix

--- src/basic/process.ts
+++ src/basic/process.ts
@@ -45,7 +45,7 @@ export class Process<T> implements Sentinel<T>, Disposable {
                 const handleD =
                     val.onTrigger?.(() =>
                         globalScheduler.add(() => {
-                            for (const se of this.state.splice(index)) {
+                            for (const se of this.state.splice(index + 1)) {
                                 se.handleD?.dispose();
                                 se.cache.dispose?.();
                             }
